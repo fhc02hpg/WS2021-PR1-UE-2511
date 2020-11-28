368e343d16f5cb0c00de7f51f2605720fff536b5
fhc02hpg <noreply@github.com>
2020-11-28 11:22:36 +0100

    missing example solutions for exercises

--- src/ArraysDemo.java
+++ src/ArraysDemo.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArraysDemo {
 
   public static void main(String[] args) {
@@ -16,6 +18,16 @@ public class ArraysDemo {
     int sum = calculateSum(test1);
     System.out.println("sum: "+ sum);
 
+    int[] reversed = reverse(test1);
+    for(int i = 0; i< reversed.length;i++) {
+      System.out.print(reversed[i]+" ");
+    }
+    System.out.println();
+
+
+    double[] test2 = {4.0,1.0,5.5,-3.75};
+    double[] stats = calcStats(test2);
+    System.out.println("min: "+stats[0] + ", max: "+stats[1]+ ", avg: "+stats[2]);
   }
 
   public static boolean containsNumber(int[] numbers, int n) {
@@ -39,6 +51,36 @@ public class ArraysDemo {
     return sum;
   }
 
+  public static int[] reverse(int[] original) {
+    int[] result = new int[original.length];
+    for(int i=0;i < result.length; i++) {
+      result[i] = original[original.length-i-1];
+    }
+    return result;
+  }
 
+  public static double[] calcStats(double[] input) {
+
+    double min = Double.MAX_VALUE;
+    double max = Double.MIN_VALUE;
+    double sum = 0.0;
+    double[] result = new double[3]; //3 elemente fuer min, max und avg
+
+    for(int d = 0;d < input.length; d++) {
+        if(input[d] < min) {
+          min = input[d];
+        }
+        if(input[d] > max) {
+          max = input[d];
+        }
+        sum += input[d];
+    }
+
+    result[0] = min;
+    result[1] = max;
+    result[2] = sum  / input.length;
+
+    return result;
+  }
 
 }
